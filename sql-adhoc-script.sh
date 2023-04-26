28c1d0df492a3ee496ffce89411e07d18192fe71
somesh2387 <116340504+somesh2387@users.noreply.github.com>
2023-04-26 18:58:40 +0530

    Create sql-adhoc-script.sh

---
+++ sql-adhoc-script.sh
@@ -0,0 +1,17 @@
+  #!/bin/bash
+  DB_URL=$(yq -r .dbUrl swat.gpg.yaml)
+  DB_USER=$(yq -r .dbUsername swat.gpg.yaml)
+  echo $DB_USER
+  DB_PASS=$(yq -r .dbPassword swat.gpg.yaml)
+  echo $DB_PASS
+  DB=$(echo "$DB_URL" | sed 's/^.*\/\([^?]*\)\?.*$/\1/')
+  echo $DB
+  regex="jdbc:mysql://([a-zA-Z0-9.-]+):[0-9]+/.*"
+	if [[ $DB_URL =~ $regex ]]; then
+    		MYSQL_URL="${BASH_REMATCH[1]}"
+    		echo "Extracted string: $extractedString"
+	else
+    		echo "Extracted string: $extractedString"
+	fi
+  echo 'logging into db $DB as $DB_USER'
+  mysql -A -h "$MYSQL_URL" -u "$DB_USER" --password="$DB_PASS" --database="$DB" < tables.sql | tee somesh.log
