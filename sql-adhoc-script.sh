c59fa325947d3a24c49092f4cb57a015730bd500
somesh2387 <116340504+somesh2387@users.noreply.github.com>
2023-05-02 13:25:01 +0530

    Update sql-adhoc-script.sh

--- sql-adhoc-script.sh
+++ sql-adhoc-script.sh
@@ -4,14 +4,6 @@
   echo $DB_USER
   DB_PASS=$(yq e .dbPassword qa-02.gpg.yaml)
   echo $DB_PASS
-  DB=$(echo "$DB_URL" | sed 's/^.*\/\([^?]*\)\?.*$/\1/')
-  echo $DB
-  regex="jdbc:mysql://([a-zA-Z0-9.-]+):[0-9]+/.*"
-	if [[ $DB_URL =~ $regex ]]; then
-    		MYSQL_URL="${BASH_REMATCH[1]}"
-    		echo "Extracted string: $extractedString"
-	else
-    		echo "Extracted string: $extractedString"
-	fi
-  echo 'logging into db $DB as $DB_USER'
+  MYSQL_URL=$1
+  MYSQL_DB=$2
   mysql -A -h "$MYSQL_URL" -u "$DB_USER" --password="$DB_PASS" --database="$DB" -e "show tables"
