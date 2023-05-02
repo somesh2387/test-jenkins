fa642fcacf2f9724ea10e0fedbf8a3a3384cc6c9
somesh2387 <116340504+somesh2387@users.noreply.github.com>
2023-05-02 13:34:01 +0530

    Update sql-adhoc-script.sh

--- sql-adhoc-script.sh
+++ sql-adhoc-script.sh
@@ -1,9 +1,11 @@
   #!/bin/bash
-  DB_URL=$(yq e .dbUrl qa-02.gpg.yaml)
-  DB_USER=$(yq e .dbUsername qa-02.gpg.yaml)
+  env =$1
+  DB_URL=$(yq e .dbUrl $env.yaml)
+  echo $env
+  DB_USER=$(yq e .dbUsername $env.gpg.yaml)
   echo $DB_USER
-  DB_PASS=$(yq e .dbPassword qa-02.gpg.yaml)
+  DB_PASS=$(yq e .dbPassword $env.gpg.yaml)
   echo $DB_PASS
-  MYSQL_URL=$1
-  MYSQL_DB=$2
-  mysql -A -h "$MYSQL_URL" -u "$DB_USER" --password="$DB_PASS" --database="$DB" -e "show tables"
+  MYSQL_URL=$2
+  MYSQL_DB=$3
+  mysql -A -h "$MYSQL_URL" -u "$DB_USER" --password="$DB_PASS" --database="$MYSQL_DB" -e "show tables"
